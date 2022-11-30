9b371e61352d313b0f5cbfaecc04245f7837a35b
Олег Сомов <somov-od@MacBook-Pro-Oleg.local>
2022-11-30 10:42:56 +0300

    add log initial question to preds file

--- seq2seq_predictor.py
+++ seq2seq_predictor.py
@@ -65,6 +65,7 @@ class Seq2SeqPredictor:
 
     def predict(self, encoder, decoder, dataloader):
         exact_match = 0
+        input_questions = []
         predicted_queries, true_queries = [], []
 
         encoder.disable_bert_training()
@@ -77,14 +78,17 @@ class Seq2SeqPredictor:
             pred_metrics = metrics.calculate_batch_metrics(target_data['original_query'], eval_result['predicted_query'])
             exact_match += pred_metrics['exact_match']
 
+            input_questions += input_data['original_question']
             predicted_queries += eval_result['predicted_query']
             true_queries += target_data['original_query']
 
         exact_match = exact_match / len(dataloader)
         result_dict = {
+
             "exact_match_score": exact_match,
             "predicted_queries": predicted_queries,
-            "true_queries": true_queries
+            "true_queries": true_queries,
+            "input_questions": input_questions
         }
         save_preds_path = os.path.join(os.environ['PROJECT_PATH'], self.config['predictions_path'],
                                        f'{self.config["inference_model_name"].split(".")[0]}_preds.json')
