5553c345c28e6fafe063b8432c15b8739e49068f
Олег Сомов <somov-od@MacBook-Pro-Oleg.local>
2023-03-16 16:49:59 +0300

    small compaund cache update

--- split_logic/tmcd_split/atom_and_compound_cache.py
+++ split_logic/tmcd_split/atom_and_compound_cache.py
@@ -6,14 +6,17 @@ from split_logic.grammar.sparql_parser import SPARQLParser
 
 class AtomAndCompoundCache:
 
-    def __init__(self, parser: SPARQLParser, query_key_name):
+    def __init__(self, parser: SPARQLParser, query_key_name, return_compound_list_flag):
         self.query_parser = parser
         self.query_to_atoms = {}
         self.query_to_compounds = {}
         self.query_key_name = query_key_name
+        self.return_compound_list = return_compound_list_flag
 
-    def get_atoms(self, query_dict):
-        query = query_dict[self.query_key_name]
+    def get_atoms(self, query_sample):
+        query = query_sample
+        if self.query_key_name is not None:
+            query = query_sample[self.query_key_name]
         if query not in self.query_to_atoms:
             atoms = self.query_parser.get_atoms(query)
             self.query_to_atoms[query] = atoms
@@ -27,15 +30,18 @@ class AtomAndCompoundCache:
             compound_list += compound_dict[compound_name]
         return compound_list
 
-    def get_compounds(self, query_dict):
-        query = query_dict[self.query_key_name]
+    def get_compounds(self, query_sample):
+        query = query_sample
+        if self.query_key_name is not None:
+            query = query_sample[self.query_key_name]
         if query not in self.query_to_compounds:
-            compounds_dict = self.query_parser.get_compounds(query)
-            self.query_to_compounds[query] = compounds_dict
+            compounds = self.query_parser.get_compounds(query)
+            self.query_to_compounds[query] = compounds
         else:
-            compounds_dict = self.query_to_compounds[query]
-        compound_list = self.extract_compounds_list(compounds_dict)
-        return compound_list
+            compounds = self.query_to_compounds[query]
+        if self.return_compound_list:
+            compounds = self.extract_compounds_list(compounds)
+        return compounds
 
     def load_cache(self, dir_path):
         self.query_to_atoms = json.load(open(os.path.join(dir_path, f'query2atom_dump.json'), 'r'))
